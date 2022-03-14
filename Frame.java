2e4f65730163604496d4349c4bdcf6cc9013dd6a
yihuai <oliver.bhbm@gmail.com>
2022-03-14 20:16:39 +0800

    v2

--- Frame.java
+++ Frame.java
@@ -12,12 +12,12 @@ public class Frame extends JFrame {
     private JMenuItem changeObjectName;
     private JMenu edit;
     private JPanel functionSelect;
-    public JToggleButton select;
-    public JToggleButton associationLine;
-    public JToggleButton generalizationLine;
-    public JToggleButton compositionLine;
-    public JToggleButton classBtn;
-    public JToggleButton useCase;
+    public JButton select;
+    public JButton associationLine;
+    public JButton generalizationLine;
+    public JButton compositionLine;
+    public JButton classBtn;
+    public JButton useCase;
 
     Frame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,96 +35,144 @@ public class Frame extends JFrame {
         menuBar = new JMenuBar();
         canvas = new Canvas(this);
         functionSelect = new JPanel();
-        select = new JToggleButton("select");
-        associationLine = new JToggleButton("association line");
-        generalizationLine = new JToggleButton("generalization line");
-        compositionLine = new JToggleButton("composition line");
-        classBtn = new JToggleButton("class");
-        useCase = new JToggleButton("use case");
+        select = new JButton("select");
+        associationLine = new JButton("association line");
+        generalizationLine = new JButton("generalization line");
+        compositionLine = new JButton("composition line");
+        classBtn = new JButton("class");
+        useCase = new JButton("use case");
         select.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "select") {
+                    select.setBackground(Color.WHITE);
                     function = "NULL";
                 } else {
                     function = "select";
+                    select.setBackground(Color.RED);
                     associationLine.setSelected(false);
                     generalizationLine.setSelected(false);
                     compositionLine.setSelected(false);
                     classBtn.setSelected(false);
                     useCase.setSelected(false);
+                    associationLine.setBackground(Color.WHITE);
+                    generalizationLine.setBackground(Color.WHITE);
+                    compositionLine.setBackground(Color.WHITE);
+                    classBtn.setBackground(Color.WHITE);
+                    useCase.setBackground(Color.WHITE);
                 }
             }
         });
+        select.setBackground(Color.WHITE);
         associationLine.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "associationLine") {
                     function = "NULL";
+                    associationLine.setBackground(Color.WHITE);
                 } else {
+                    associationLine.setBackground(Color.RED);
                     function = "associationLine";
                     select.setSelected(false);
                     generalizationLine.setSelected(false);
                     compositionLine.setSelected(false);
                     classBtn.setSelected(false);
                     useCase.setSelected(false);
+                    select.setBackground(Color.WHITE);
+                    generalizationLine.setBackground(Color.WHITE);
+                    compositionLine.setBackground(Color.WHITE);
+                    classBtn.setBackground(Color.WHITE);
+                    useCase.setBackground(Color.WHITE);
                 }
             }
         });
+        associationLine.setBackground(Color.WHITE);
         generalizationLine.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "generalizationLine") {
+                    generalizationLine.setBackground(Color.WHITE);
                     function = "NULL";
                 } else {
+                    generalizationLine.setBackground(Color.RED);
                     function = "generalizationLine";
                     associationLine.setSelected(false);
                     select.setSelected(false);
                     compositionLine.setSelected(false);
                     classBtn.setSelected(false);
                     useCase.setSelected(false);
+                    select.setBackground(Color.WHITE);
+                    associationLine.setBackground(Color.WHITE);
+                    compositionLine.setBackground(Color.WHITE);
+                    classBtn.setBackground(Color.WHITE);
+                    useCase.setBackground(Color.WHITE);
                 }
             }
         });
+        generalizationLine.setBackground(Color.WHITE);
         compositionLine.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "compositionLine") {
+                    compositionLine.setBackground(Color.WHITE);
                     function = "NULL";
                 } else {
+                    compositionLine.setBackground(Color.RED);
                     function = "compositionLine";
                     associationLine.setSelected(false);
                     generalizationLine.setSelected(false);
                     select.setSelected(false);
                     classBtn.setSelected(false);
                     useCase.setSelected(false);
+                    select.setBackground(Color.WHITE);
+                    associationLine.setBackground(Color.WHITE);
+                    generalizationLine.setBackground(Color.WHITE);
+                    classBtn.setBackground(Color.WHITE);
+                    useCase.setBackground(Color.WHITE);
                 }
             }
         });
+        compositionLine.setBackground(Color.WHITE);
         classBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "class") {
+                    classBtn.setBackground(Color.WHITE);
                     function = "NULL";
                 } else {
+                    classBtn.setBackground(Color.RED);
                     function = "class";
                     associationLine.setSelected(false);
                     generalizationLine.setSelected(false);
                     compositionLine.setSelected(false);
                     select.setSelected(false);
                     useCase.setSelected(false);
+                    select.setBackground(Color.WHITE);
+                    associationLine.setBackground(Color.WHITE);
+                    generalizationLine.setBackground(Color.WHITE);
+                    compositionLine.setBackground(Color.WHITE);
+                    useCase.setBackground(Color.WHITE);
                 }
             }
         });
+        classBtn.setBackground(Color.WHITE);
         useCase.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "useCase") {
+                    useCase.setBackground(Color.WHITE);
                     function = "NULL";
                 } else {
+                    useCase.setBackground(Color.RED);
                     function = "useCase";
                     associationLine.setSelected(false);
                     generalizationLine.setSelected(false);
                     compositionLine.setSelected(false);
                     classBtn.setSelected(false);
                     select.setSelected(false);
+                    select.setBackground(Color.WHITE);
+                    associationLine.setBackground(Color.WHITE);
+                    generalizationLine.setBackground(Color.WHITE);
+                    compositionLine.setBackground(Color.WHITE);
+                    classBtn.setBackground(Color.WHITE);
                 }
             }
         });
+        useCase.setBackground(Color.WHITE);
         ButtonGroup.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (function == "select") {
