d9a880d91d84c68f1d4038ce1e0cd5660cd81a40
yihuai <oliver.bhbm@gmail.com>
2022-03-18 17:42:11 +0800

    小改進

---
+++ FunctionButton.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class FunctionButton extends JButton {
+    Frame frame;
+
+    FunctionButton(String s, Frame frame) {
+        this.setText(s);
+        this.frame = frame;
+        this.setOpaque(true);
+        this.setBackground(Color.WHITE);
+        this.setForeground(Color.BLACK);
+        this.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                changeFunction();
+            }
+        });
+    }
+
+    void changeFunction() {
+        frame.changeFunction(this);
+    }
+}
