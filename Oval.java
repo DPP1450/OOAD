6beae6453c6686379fe7bc5a5c57919e8c98ec07
DPP1450 <yihuai@msp.csie.ncu.edu.tw>
2022-03-27 16:06:30 +0800

    小改

--- Oval.java
+++ Oval.java
@@ -7,10 +7,10 @@ public class Oval extends Block {
         this.setSize(80, 60);
         super.title.setSize(60, 20);
         super.title.setLocation(25, 15);
-        super.p1.setLocation(0, 30);
-        super.p2.setLocation(70, 30);
-        super.p3.setLocation(30, 0);
-        super.p4.setLocation(30, 50);
+        super.p1.setLocation(30, 0);
+        super.p2.setLocation(30, 50);
+        super.p3.setLocation(0, 30);
+        super.p4.setLocation(70, 30);
         super.p5.setLocation(70, 50);
         super.height = 60;
         super.width = 80;
