dff02a89cbb4408c45a176bd7ea9c66859a1aaeb
DPP1450 <oliver870623@kimo.com>
2022-06-06 23:16:58 +0800

    更改cavas/UmlFrame為singleton clas

--- Uml.java
+++ Uml.java
@@ -2,8 +2,8 @@ public class Uml {
     public static void main(String[] args) {
         UmlFrame.getInstance();
         UmlFrame.getInstance().newItem(); // 新增frame 上的物件
-        UmlFrame.getInstance().addItem(); // 把他加到frame 中
-        UmlFrame.getInstance().initLayout(); // 佈局
-        UmlFrame.getInstance().groupAddActionListener();
+        UmlFrame.getInstance().addItem(); // 將上面新增的物件加到frame 中
+        UmlFrame.getInstance().initLayout(); // 初始化佈局
+        UmlFrame.getInstance().groupAddActionListener(); // 新增menubar item的ActionListener
     }
 }
